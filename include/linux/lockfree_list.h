974fcb3595bce0e4a4e4232ffbc4162c6b48c12e
joohyun kyong <joohyun0115@gmail.com>
2015-07-01 14:21:52 +0900

    lockfree list: add inital version of lock free list

---
+++ include/linux/lockfree_list.h
@@ -0,0 +1,83 @@
+#ifndef _LOCKFREE_LIST_H_
+#define _LOCKFREE_LIST_H_
+
+#include <linux/kernel.h>
+#include <asm/cmpxchg.h>
+
+struct lockfree_list_head {
+    struct lockfree_list_node *head, *tail;
+};
+
+struct lockfree_list_node {
+    struct lockfree_list_node *next;
+    void *key;
+};
+
+#define LOCKFREE_LIST_HEAD_INIT(name)   { NULL, NULL }
+#define LOCKFREE_LIST_HEAD(name) \
+		struct lockfree_list_head name = LOCKFREE_LIST_HEAD_INIT(name)
+
+#define LOCKFREE_LIST_HEAD_NODE_INIT(name)   { NULL, NULL }
+#define LOCKFREE_LIST_HEAD_NODE(name) \
+		struct lockfree_list_node name = LOCKFREE_LIST_HEAD_NODE_INIT(name)
+
+#define LOCKFREE_LIST_TAIL_NODE_INIT(name)   { NULL, NULL }
+#define LOCKFREE_LIST_TAIL_NODE(name) \
+		struct lockfree_list_node name = LOCKFREE_LIST_TAIL_NODE_INIT(name)
+
+#define LOCKFREE_LIST_SAVE_KEY(name, member) \
+		(name)->member.key = (void *)(name)
+
+static inline void init_lockfree_list_head(struct lockfree_list_head *list,
+        struct lockfree_list_node *head, struct lockfree_list_node *tail)
+{
+    head->next = tail;
+    list->head = head;
+    list->tail = tail;
+}
+
+#define lockfree_list_entry(ptr, type, member)      \
+    container_of(ptr, type, member)
+
+#define lockfree_list_for_each(pos, node)           \
+    for ((pos) = (node); pos; (pos) = (pos)->next)
+
+#define lockfree_list_for_each_entry(pos, node, member)             \
+    for ((pos) = lockfree_list_entry((node), typeof(*(pos)), member);   \
+         &(pos)->member != NULL;                    \
+         (pos) = lockfree_list_entry((pos)->member.next, typeof(*(pos)), member))
+
+#define lockfree_list_for_each_entry_safe(pos, n, node, member)               \
+    for (pos = lockfree_list_entry((node), typeof(*pos), member);             \
+         &pos->member != NULL &&                           \
+         (n = lockfree_list_entry(pos->member.next, typeof(*n), member), true); \
+         pos = n)
+
+static inline bool lockfree_list_empty(const struct lockfree_list_head *head)
+{
+	return ACCESS_ONCE(head->head->next) == ACCESS_ONCE(head->tail);
+}
+
+static inline struct lockfree_list_node *lockfree_list_next(
+		struct lockfree_list_node *node)
+{
+	return node->next;
+}
+
+extern bool lockfree_list_add_batch(struct lockfree_list_node *new,
+		struct lockfree_list_head *head);
+static inline bool lockfree_list_add(struct lockfree_list_node *new,
+		struct lockfree_list_head *head)
+{
+	return lockfree_list_add_batch(new, head);
+}
+
+struct lockfree_list_node *lockfree_list_del_batch(
+		struct lockfree_list_node *node, struct lockfree_list_head *head);
+static inline struct lockfree_list_node *lockfree_list_del(
+		struct lockfree_list_node *node, struct lockfree_list_head *head)
+{
+	return lockfree_list_del_batch(node, head);
+}
+
+#endif /* #ifndef _LOCKFREE_LIST_H_ */
---
+++ lib/lockfree_list.c
@@ -0,0 +1,104 @@
+#include <linux/kernel.h>
+#include <linux/export.h>
+#include <linux/interrupt.h>
+#include <linux/lockfree_list.h>
+
+static inline int is_marked_ref(long i)
+{
+	return (i & 0x1L);
+}
+
+static inline long get_unmarked_ref(long w)
+{
+	return (w & ~0x1L);
+}
+
+static inline long get_marked_ref(long w)
+{
+	return (w | 0x1L);
+}
+
+static struct lockfree_list_node *list_search(struct lockfree_list_head *head, void *key,
+		struct lockfree_list_node **left_node)
+{
+	struct lockfree_list_node *left_node_next = head->head->next;
+	struct lockfree_list_node *right_node = head->head->next;
+
+search_again:
+	do {
+		struct lockfree_list_node *t = head->head;
+		struct lockfree_list_node *t_next = head->head->next;
+
+		/* Find left_node and right_node */
+		do {
+			if (!is_marked_ref((long)t_next)) {
+				(*left_node) = t;
+				left_node_next = t_next;
+			}
+			t = (struct lockfree_list_node *) get_unmarked_ref((long)t_next);
+			if (t == head->tail)
+				break;
+			t_next = t->next;
+		} while (is_marked_ref((long)t_next) || (t->key < key));
+		right_node = t;
+
+		/* Check that nodes are adjacent */
+		if (left_node_next == right_node) {
+			if ((right_node != head->tail) && is_marked_ref((long) right_node->next))
+				goto search_again;
+			else
+				return right_node;
+		}
+
+		/* Remove one or more marked nodes */
+		if (cmpxchg(&(*left_node)->next, left_node_next, right_node)) {
+			if ((right_node != head->tail) && is_marked_ref((long) right_node->next))
+				goto search_again;
+			else
+				return right_node;
+		}
+
+	} while (1);
+}
+
+bool lockfree_list_add_batch(struct lockfree_list_node *new,
+		struct lockfree_list_head *head)
+{
+	struct lockfree_list_node *right_node, *left_node;
+
+	do {
+		right_node = list_search(head, new->key, &left_node);
+		if ((right_node != head->tail) && (right_node->key == new->key))
+			return false;
+
+		new->next = right_node;
+		smp_mb();
+	} while (cmpxchg(&left_node->next, right_node, new) != right_node);
+
+	return true;
+}
+EXPORT_SYMBOL_GPL(lockfree_list_add_batch);
+
+struct lockfree_list_node *lockfree_list_del_batch(
+		struct lockfree_list_node *node, struct lockfree_list_head *head)
+{
+	struct lockfree_list_node *right_node, *right_node_next, *left_node;
+
+	do {
+		right_node = list_search(head, node->key, &left_node);
+		if ((right_node == head->tail) || (right_node->key != node->key)) {
+			printk(KERN_ALERT "lockfree_list del fail");
+			return NULL;
+		}
+		right_node_next = right_node->next;
+		if (!is_marked_ref((long) right_node_next))
+			if (cmpxchg(&right_node->next, right_node_next,
+					(struct lockfree_list_node *)get_marked_ref((long) right_node_next)) == right_node_next)
+				break;
+	} while (1);
+	if (cmpxchg(&left_node->next, right_node, right_node_next) != right_node)
+		right_node = list_search(head, right_node->key, &left_node);
+
+	return right_node;
+}
+EXPORT_SYMBOL_GPL(lockfree_list_del_batch);
